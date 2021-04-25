cb0a93942c6278a7ea724b450f5a9e566e450c35
Unais Siddiqui <unaisyoushasiddiqui@gmail.com>
2021-04-25 15:35:17 +0500

    backend completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corshreaders',
+    'corsheaders',
     'rest_framework',
     'todo',
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+
+
